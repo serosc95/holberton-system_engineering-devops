6fbde22f3cceb89f123f9d4809257d391870d690
serosc95 <oscardleon95@gmail.com>
2021-02-02 01:08:20 +0000

    get data users and all form API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    user_id = argv[1]
+
+    url_todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'
+    url_user = 'https://jsonplaceholder.typicode.com/users/{}'
+    resp_todos = requests.get(url_todo.format(user_id))
+    resp_user = requests.get(url_user.format(user_id))
+    json_r_todos = resp_todos.json()
+    json_r_user = resp_user.json()
+
+    completed_tasks = 0
+    user_name = json_r_user.get("name")
+
+    total_tasks = len(json_r_todos)
+    filteredData = list(filter(lambda d: d['completed'] is True, json_r_todos))
+    completed_tasks = len(filteredData)
+
+    print("Employee {} is done with tasks({}/{}):".format(user_name,
+                                                          completed_tasks,
+                                                          total_tasks))
+
+    for dic in filteredData:
+        print("\t {}".format(dic.get("title")))
