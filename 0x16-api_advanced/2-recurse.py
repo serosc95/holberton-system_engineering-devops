7c66e5acd51c7bd5b846eff8f320e6845c2c042a
serosc95 <oscardleon95@gmail.com>
2021-02-05 02:15:39 +0000

    list containing the titles of all hot articles

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,16 +4,18 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=""):
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "MyBot/0.0.1"}
-    params = {"after": after}
-    res = requests.get(url, headers=headers, params=params)
-    if res.status_code == 404:
+    usr = 'Opera/9.80 (X11; Linux i686; U; ru) Presto/2.8.131 Version/11.11'
+    usrAg = {'User-Agent': usr}
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit,
+                                                                 after)
+    response = requests.get(url, headers=usrAg)
+    json_r = response.json()
+    if response.status_code == 200:
+        for post in json_r['data']['children']:
+            hot_list.append(post['data']['title'])
+        new_after = json_r['data']['after']
+        if new_after is None:
+            return hot_list
+        return recurse(subreddit, hot_list, new_after)
+    else:
         return None
-    json_r = res.json().get("data")
-    for post in json_r['data']['children']:
-        hot_list.append(post['data']['title'])
-    new_after = json_r['data']['after']
-    if new_after is None:
-        return hot_list
-    return recurse(subreddit, hot_list, new_after)
